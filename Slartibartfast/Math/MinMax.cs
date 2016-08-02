3a59ad113a7fdc033346dba165be6a36efb4cdf9
MaxPlay <maxplayyt@gmail.com>
2016-08-02 03:17:36 +0200

    Habitable zones
    Added class for min-max value management
    Added calculations for habitable zones

---
+++ Slartibartfast/Math/MinMax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slartibartfast.Math
+{
+    public class MinMax<T>
+    {
+        private T min;
+
+        public T Min
+        {
+            get { return min; }
+            set { min = value; }
+        }
+
+        private T max;
+
+        public T Max
+        {
+            get { return max; }
+            set { max = value; }
+        }
+
+        public MinMax(T max)
+        {
+            this.max = max;
+        }
+
+        public MinMax(T min, T max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+    }
+}
--- Slartibartfast/Planets/Sun.cs
+++ Slartibartfast/Planets/Sun.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Slartibartfast.Math;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,50 @@ using System.Threading.Tasks;
 
 namespace Slartibartfast.Planets
 {
+    public enum SpectralClass
+    {
+        B,
+        A,
+        F,
+        G,
+        K,
+        M
+    }
+
     public class Sun
     {
         public const float SOL_RADIUS = 1;
         public const float SOL_MASS = 1;
-        public const float SOL_LUMINOSITY = 1;
         public const float SOL_ABSOLUTE_MAGNITUDE = 4.83f;
+        public const float SOL_INNER_CHZ = 0.95f;
+        public const SpectralClass SOL_SPECTRAL_CLASS = SpectralClass.G;
+
+
+        /// <summary>
+        /// Returns the Bolometric Correction Constant of the corresponding Spectral Class. The spectral class of the sun is G.
+        /// </summary>
+        /// <param name="SpectralClass">The spectral class for the bolometric correction value.</param>
+        /// <returns></returns>
+        public static float BolometricCorrectionConstant(SpectralClass SpectralClass)
+        {
+            switch (SpectralClass)
+            {
+                case SpectralClass.B:
+                    return -2f;
+                case SpectralClass.A:
+                    return -0.3f;
+                case SpectralClass.F:
+                    return -0.15f;
+                case SpectralClass.G:
+                    return -0.4f;
+                case SpectralClass.K:
+                    return -0.8f;
+                case SpectralClass.M:
+                    return -2f;
+                default:
+                    return 0;
+            }
+        }
 
         private float radius;
 
@@ -35,17 +74,6 @@ namespace Slartibartfast.Planets
             set { mass = value; }
         }
 
-        private float luminosity;
-
-        /// <summary>
-        /// The luminosity of the sun.
-        /// </summary>
-        public float Luminosity
-        {
-            get { return luminosity; }
-            set { luminosity = value; }
-        }
-
         private float absoluteMagnitude;
 
         /// <summary>
@@ -58,5 +86,27 @@ namespace Slartibartfast.Planets
             set { absoluteMagnitude = value; }
         }
 
+        private SpectralClass spectralClass;
+
+        public SpectralClass SpectralClass
+        {
+            get { return spectralClass; }
+            set { spectralClass = value; }
+        }
+
+        /// <summary>
+        /// Calculates the habitable zone for the sun in AU.
+        /// </summary>
+        /// <returns></returns>
+        public MinMax<float> GetHabitableZone()
+        {
+            float correctedBolometricMagnitude = this.absoluteMagnitude - BolometricCorrectionConstant(this.spectralClass);
+            float absoluteLuminosity = (float)System.Math.Pow(10, (correctedBolometricMagnitude - 4.72) / -2.5f);
+
+            float min = (float)System.Math.Sqrt(absoluteLuminosity / 1.1);
+            float max = (float)System.Math.Sqrt(absoluteLuminosity / 0.53);
+
+            return new MinMax<float>(min, max);
+        }
     }
 }
